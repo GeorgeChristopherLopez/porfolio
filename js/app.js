98e1c1f569f8ddfe53b0e35b3b20556bf18387dc
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-01-08 23:09:41 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+
 let bg = document.querySelector('canvas');
 
+let projectBtn = document.querySelector('.project-btn');
 
+let app = document.querySelector('.app');
+let projects = document.querySelector('.projects');
+projectBtn.addEventListener('click', e => {
+    projects.classList.toggle('hide');
+    app.style.marginTop = "0";
+})
 
 
 console.log(bg);
